25de108ba751ecfd475444c6327a52e0c4e67daf
folicri27 <folicri278@g.educaand.es>
2025-02-13 14:19:09 +0100

    Se ha agregado la estructura del programa

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //VARIABLE
+            //ENTRADA
+            //PROCESO
+            //SALIDA
         }
     }
 }
